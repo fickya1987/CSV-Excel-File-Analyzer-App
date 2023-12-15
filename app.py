ebe3659613654d76dcd9611d652bf119bf15df8d
Kheem-Dh <dharmanikheem@gmail.com>
2023-12-15 13:09:19 -0800

    User authentication with 4 more vizs

--- app.py
+++ app.py
@@ -1,16 +1,59 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+import plotly.express as px
+from streamlit_authenticator import Authenticate
+import random
+import string
 
-# Set the title of the app
-st.title('CSV/Excel File Analyzer')
+# Function to generate a random string to use as a secret key
+def generate_secret_key(length=24):
+    return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length))
 
-# Create a file uploader to accept CSV or Excel files
-uploaded_file = st.file_uploader("Choose a CSV or Excel file", type=['csv', 'xlsx'])
+# Assuming you've already hashed the passwords using a separate script
+users = {
+    "user1": {
+        "name": "Alice",
+        "username": "alice",
+        "password": "$2b$12$cp1Eo2n2WxcjmWznnPjbjerdSxmbKzEV8YmYSSf/vpOg1LCj0oH4i",  # Replace with actual hashed value
+        "email": "alice@example.com"
+    },
+    "user2": {
+        "name": "Bob",
+        "username": "bob",
+        "password": "$2b$12$.ZpsS3X7m9GA3rm3PakOdejxKbvgCjBHo51WSJOFkRJq7HKDkqdBa",  # Replace with actual hashed value
+        "email": "bob@example.com"
+    }
+}
 
-if uploaded_file is not None:
-    try:
+# Format the credentials as expected by streamlit-authenticator
+credentials = {
+    "usernames": {
+        user_detail["username"]: {
+            "name": user_detail["name"],
+            "password": user_detail["password"],
+            "email": user_detail["email"]
+        } for user_detail in users.values()
+    }
+}
+
+# Define a cookie name and a secret key
+cookie_name = "streamlit_authenticator"
+secret_key = generate_secret_key()
+
+# Create an authentication object with the corrected format
+authenticator = Authenticate(credentials, cookie_name, secret_key)
+
+# Start the authentication process
+name, authentication_status, username = authenticator.login('Login', 'main')
+
+if authentication_status:
+    authenticator.logout('Logout', 'main')
+    st.title('Welcome to CSV/Excel File Analyzer App')
+
+    # File upload and data visualization functionalities here
+    uploaded_file = st.file_uploader("Choose a CSV or Excel file", type=['csv', 'xlsx'])
+
+    if uploaded_file is not None:
         # Read the file
         if uploaded_file.name.endswith('.csv'):
             df = pd.read_csv(uploaded_file, encoding='utf-8')
@@ -23,31 +66,33 @@ if uploaded_file is not None:
         # Select columns for visualization
         st.subheader('Select Columns for Visualization')
         all_columns = df.columns.tolist()
-        selected_columns = st.multiselect('Select two columns', all_columns, default=all_columns[:2])
+        selected_columns = st.multiselect('Select columns', all_columns, default=all_columns[:2])
         
-        if len(selected_columns) != 2:
-            st.error('Please select exactly two columns for visualization.')
+        if len(selected_columns) < 2:
+            st.error('Please select at least two columns for visualization.')
         else:
-            # Preprocessing selected columns
-            df_vis = df[selected_columns].dropna()
-            df_vis = df_vis.apply(pd.to_numeric, errors='coerce').dropna()
-
             # Visualization options
-            plot_type = st.radio("Select Plot Type", ('Line Plot', 'Histogram'))
+            plot_type = st.selectbox("Select Plot Type", ('Line Plot', 'Bar Chart', 'Scatter Plot', 'Histogram'))
 
-            # Visualization: Line Plot
-            if plot_type == 'Line Plot' and st.button('Show Line Plot'):
-                plt.figure(figsize=(10, 4))
-                sns.lineplot(data=df_vis)
-                st.pyplot(plt)
+            # Generate and display the plot based on the selected type and columns
+            if plot_type == 'Line Plot':
+                fig = px.line(df, x=selected_columns[0], y=selected_columns[1])
+                st.plotly_chart(fig)
 
-            # Visualization: Histogram
-            if plot_type == 'Histogram' and st.button('Show Histogram'):
-                plt.figure(figsize=(10, 4))
-                for col in selected_columns:
-                    sns.histplot(df_vis[col], kde=True, label=col)
-                plt.legend()
-                st.pyplot(plt)
+            elif plot_type == 'Bar Chart':
+                fig = px.bar(df, x=selected_columns[0], y=selected_columns[1])
+                st.plotly_chart(fig)
 
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
+            elif plot_type == 'Scatter Plot':
+                fig = px.scatter(df, x=selected_columns[0], y=selected_columns[1])
+                st.plotly_chart(fig)
+
+            elif plot_type == 'Histogram':
+                fig = px.histogram(df, x=selected_columns[0], y=selected_columns[1])
+                st.plotly_chart(fig)
+
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
